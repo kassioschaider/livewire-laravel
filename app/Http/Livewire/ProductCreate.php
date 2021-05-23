5f5b105950af4d9434eaaa3b317f30f9f42a11e3
Kassio <kassio.schaider@hotmail.com>
2021-05-23 01:06:28 -0300

    Init livewire

--- app/Http/Livewire/ProductCreate.php
+++ app/Http/Livewire/ProductCreate.php
@@ -6,6 +6,20 @@ use Livewire\Component;
 
 class ProductCreate extends Component
 {
+    public $barCode;
+    public $name;
+
+    protected $rules = [
+        'barCode' => 'required',
+        'name' => 'required',
+    ];
+
+    public function create()
+    {
+        $this->validate();
+        dd($this->name, $this->barCode);
+    }
+
     public function render()
     {
         return view('livewire.product-create');
--- resources/views/livewire/product-create.blade.php
+++ resources/views/livewire/product-create.blade.php
@@ -1,3 +1,26 @@
 <div>
+    <h2>Create Product</h2>
 
+    <form wire:submit.prevent="create" method="post">
+
+        <label for="barCode">Bar Code</label>
+
+        <input type="text" wire:model="barCode">
+        <div class="error-message">
+            @error('barCode')
+            {{$message}}
+            @enderror
+        </div>
+
+        <label for="name">Name</label>
+
+        <input type="text" wire:model="name">
+        <div class="error-message">
+            @error('name')
+            {{$message}}
+            @enderror
+        </div>
+
+        <button class="btn btn-primary">Add</button>
+    </form>
 </div>
